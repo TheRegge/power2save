fbe411c892de6954c69f7c834a75f304c051d1d8
Regis Zaleman <regis.zaleman@gmail.com>
2022-07-06 13:18:02 -0400

    Add JSDoc comments

--- power2save.js
+++ power2save.js
@@ -1,6 +1,16 @@
+/** Class Power2Save */
 export default class Power2Save {
+  /**
+   * Internal array to store the power of 2 data
+   * @private
+   */
   power2Data = []
 
+  /**
+   * Constructor for the Power2Save class
+   *
+   * @param {Array} data The data to initialize the class with. The persisted data will be chosen from this array.
+   */
   constructor(data) {
     this.power2Data = []
     let index = 1
@@ -10,6 +20,12 @@ export default class Power2Save {
     }
   }
 
+  /**
+   * Get the items in power2data that match the provided value, which is a sum of powers of 2.
+   *
+   * @param {number} val The value to get the items from
+   * @returns {Array} The items that match the value
+   */
   getItems(val) {
     const maxValue = Math.pow(2, this.power2Data.filter((n) => n).length)
     const items = []
@@ -22,6 +38,13 @@ export default class Power2Save {
     return items.reverse()
   }
 
+  /**
+   * Get the sum of powers of 2 that represents the items.
+   * This is the data to be persisted.
+   *
+   * @param {number} val The value to get the items from
+   * @returns {Array} The items that match the value
+   */
   getValue(items) {
     let val = 0
     const cache = []
